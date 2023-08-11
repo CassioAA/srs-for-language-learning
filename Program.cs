aa200ccce2c0ff220fe8db2add653aba4f53630a
CassioAA <cassioalmeidaccti@gmail.com>
2023-08-11 10:02:35 -0300

    simple validation

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string? sentence = "";
+
+do {
+
+    Console.WriteLine("What does the front of the card say?");
+
+    sentence = Console.ReadLine();
+
+} while(!IsValid(sentence));
+
+Console.WriteLine(sentence);
+
+bool IsValid(string? sentence) {
+
+    if (string.IsNullOrWhiteSpace(sentence)) {
+
+        Console.WriteLine("The front of the card can't be empty.");
+        Console.WriteLine();
+        
+        return false;
+
+    }
+
+    return true;
+    
+}
